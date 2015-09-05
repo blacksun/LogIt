77e8e10fea74d0ca7dda48b60923fb3f07d17383
blacksun <gab.santini@gmail.com>
2015-09-05 18:21:15 +0200

    add db scripts

---
+++ LogItServer/data/sql/fixtures.sql
@@ -0,0 +1,6 @@
+INSERT INTO 
+	logs 	(content)
+    VALUES  ('prova 1 2 3 prova');
+INSERT INTO 
+	logs 	(content)
+    VALUES  ('test');
---
+++ LogItServer/data/sql/logit.sql
@@ -0,0 +1,5 @@
+ CREATE TABLE logs (
+   id int NOT NULL auto_increment,
+   content varchar(255) NOT NULL,
+   PRIMARY KEY (id)
+ );
